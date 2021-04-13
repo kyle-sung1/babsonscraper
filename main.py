f5566284573f0d04142ff04bd8f8f5d07d7e3c80
Kyle Sung <64438025+kyle-sung1@users.noreply.github.com>
2021-04-13 19:30:18 -0400

    Update main.py
    TODO: we could run scheduling, so that you cannot call getPeriod("breakfast") past breakfast, since that raises an error. Instead, add 1 to the date so that it returns the breakfast menu for tomorrow. Or we could return an error like "Cant look at breakfast menu past breakfast"

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """
 Script that scrapes Trim dining hall at Babson and prints food + calories as strings
+
+Note: can only run each period before specific meal period, or else error is raised
 """
 import time
 from datetime import date
@@ -7,18 +9,24 @@ import random
 import requests
 import json
 
-class Food: #TODO: make the right attributes, ie ingredients and nutrients (which can be a dict), and filters (vegetarian, vegan etc)
-    def __init__(self, id, name, type="", allergens="", calories=""):
+class Food: 
+    """
+    class Food, where attributes name, id and calories are string, and nutrients and filters are lists of strings
+    """
+    def __init__(self, id, name, ingredients="", nutrients=[], filters=[], calories=""):
         self.id = id
         self.name = name
-        self.type = type
-        self.allergens = allergens
+        self.ingredients = ingredients
+        self.nutrients = nutrients
+        self.filters = filters
         self.calories = calories
     def __str__(self):
         return '{self.name} has {self.calories} calories'.format(self=self)
 
-
-def getBreakfast():
+def getPeriod(period=None):
+    """
+    gets the period path for the meal period that is passed as a string parameter. returns period path as a string.
+    """
     headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Encoding': 'gzip, deflate, br',
@@ -28,37 +36,65 @@ def getBreakfast():
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
     }
     today = date.today()
-    bf = "https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/607505c13a585b19a5fed9b5?platform=0&date=2021-4-13"
-    # bf somehow returns json with all the ids for every meal period, could be useful
-    # the id is then plugged into .../periods/:id?platform=0...
-    # not sure if id changes or something
-    # once that is figured out we can concatenate today to the end -> &date=" + today
+    url = "https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/607505c13a585b19a5fed9b5?platform=0&date=" + str(today)
+    response = requests.get(url, headers=headers) # send request with headers
+    json_data = json.loads(response.text) # decode json that is returned into parsable dictionary
+    # return the period id for each of the meal periods
+    if period.lower() == "breakfast":
+        for item in json_data['periods']:
+            if item['name'] == 'Breakfast':
+                return item['id']
+    elif period.lower() == "lunch":
+        for item in json_data['periods']:
+            if item['name'] == 'Lunch':
+                return item['id']
+    elif period.lower() == "dinner":
+        for item in json_data['periods']:
+            if item['name'] == 'Dinner':
+                return item['id']
+    else:
+        print("no period selected") # or we could raise exception actually
 
-    response = requests.get("https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/6075221e5e42ad1ff3422dcb?platform=0&date=2021-4-13", headers=headers)
+
+def getFoodItems(periodID):
+    """
+    returns a list of Food objects given the periodID parameter that is returned from getPeriod(). 
+    """
+    headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Host': 'api.dineoncampus.com',
+    'Origin': 'https://dineoncampus.com',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+    }
+    today = date.today()
+    url = "https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/" + periodID + "?platform=0&date=" + str(today)
+
+    response = requests.get(url, headers=headers) # send request wuth headers
     #print(response.text)
-    json_data = json.loads(response.text)
+    json_data = json.loads(response.text) # decode json
     #print(json_data)
-    print(json_data["status"])
-
+    if json_data["status"] != "success":
+        raise Exception("Status not successful") # raise exception if status returned is not successful
 
     bfList = []
     for category in json_data['menu']['periods']['categories']:
-        for foodName in category['items']:
-            bfList.append(Food(foodName['id'], foodName['name'], calories=foodName['calories'])) # append ingredients also
-            # TODO: iterate through nutrients
-            # for nutrients in foodName['nutrients']:
-            # TODO: iterate through filters
-            # for filters in foodName['filters']:
+        for foodName in category['items']: # assign attributes to each food item on the menu
+            nutrients = [dict["name"] + ":" + dict["value"] + "(" + dict["uom"] + ")" for dict in foodName['nutrients']]
+            filters = [dict["type"] + ":" + dict["name"] for dict in foodName['filters']]
+            bfList.append(Food(foodName['id'], foodName['name'], nutrients=nutrients, ingredients=foodName['ingredients'], filters= filters, calories=foodName['calories']))
+
     print(len(bfList))
-    for item in bfList:
-        print(str(item))
+    #for item in bfList:
+        #print(str(item))
+        #pass
+    #print(bfList[0].nutrients)
+    #print(bfList[0].ingredients)
+    #print(bfList[0].filters)
 
 
-def getLunch():
-    lunch = "https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/607505c13a585b19a5fed9bf?platform=0&date=2021-4-13"
-    pass
-def getDinner():
-    dinner = "https://api.dineoncampus.com/v1/location/5880da183191a21e8af61adc/periods/607505c13a585b19a5fed9b8?platform=0&date=2021-4-13"
-    pass
+
 if __name__ == '__main__':
-    getBreakfast()
+    #getBreakfast()
+    getFoodItems(getPeriod("dinner"))
